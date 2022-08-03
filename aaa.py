2dd04d5d68484d49fa0ddff730c38f584b83897f
Liron Kruchinin <54192894+LilKruchi@users.noreply.github.com>
2022-08-03 22:09:27 +0300

    Write size of chips on picture

--- aaa.py
+++ aaa.py
@@ -1,23 +1,75 @@
-import cv2 as cv#read the image
-img = cv.imread("pat.jpeg")
-#convert the image to grayscale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+import cv2
+import numpy as np
+import math
 
-#blur image to reduce the noise in the image while thresholding. #This smoothens the sharp edges in the image.
+originalImage = cv2.imread('photos/2008924x10.3.jpg', cv2.IMREAD_UNCHANGED)
+grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+imageScalePercent = 80
 
-blur = cv.blur(gray, (10,10))
 
-#Apply thresholding to the image
+# creates black and white image
+# grayImage, imports the photo
+# sensitivity of the white : 
+# 0 - Ultra sensitive    255 - not sensitive
+# best value 127... for low res photos 135 works best
+(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 135, 255, cv2.THRESH_BINARY) 
 
-ret, thresh = cv.threshold(blur, 1, 255, cv.THRESH_OTSU)
-#find the contours in the image
-contours, heirarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-#draw the obtained contour lines(or the set of coordinates forming a line) on the original image
-cv.drawContours(img, contours, -1, (0,255,0), 5)
-#show the image
-cv.namedWindow('Contours',cv.WINDOW_NORMAL)
-cv.namedWindow('Thresh',cv.WINDOW_NORMAL)
-cv.imshow('Contours', img)
-cv.imshow('Thresh', thresh)
-cv.waitKey(0)
-cv.destroyAllWindows()
+kernel=np.ones((2,2),np.uint8)
+dilated=cv2.dilate(blackAndWhiteImage, kernel, iterations=1)
+
+
+
+# original image rescaling
+width = int(originalImage.shape[1] * imageScalePercent / 100)
+height = int(originalImage.shape[0] * imageScalePercent / 100)
+dim = (width, height)
+originalImage = cv2.resize(originalImage, dim, interpolation = cv2.INTER_AREA)
+
+#gray image rescale
+width = int(grayImage.shape[1] * imageScalePercent / 100)
+height = int(grayImage.shape[0] * imageScalePercent / 100)
+dim = (width, height)
+grayImage = cv2.resize(grayImage, dim, interpolation = cv2.INTER_AREA)
+
+# black and white rescale
+width = int(dilated.shape[1] * imageScalePercent / 100)
+height = int(dilated.shape[0] * imageScalePercent / 100)
+dim = (width, height)
+dilated = cv2.resize(dilated, dim, interpolation = cv2.INTER_AREA)
+
+
+
+contours, heirarchy = cv2.findContours(dilated, 
+                                       cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(originalImage, contours, -1, (0,0,0), 1)
+contours=[cv2.boundingRect(cnt) for cnt in contours]
+
+for cnt in contours:
+   x,y,w,h = cnt
+   x2 = w + x
+   x1 = x 
+   y1 = y
+   y2 = h + y
+   XYDistance = math.dist([x2,y2], [x1,y1])
+   XYDistance = round(XYDistance, 4)
+   if XYDistance > 11.5:
+    cv2.rectangle(originalImage, (x,y-4),(x+w,y+h), (255,155,0),1)
+    cv2.putText(originalImage, str(XYDistance), (x, y-9), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1)
+#    print(x, y)
+#    print(w, h)
+#    print(x+w, y+h)
+#    print(XYDistance)
+#    print("____________")
+
+cv2.imshow('Gray image', grayImage)
+cv2.imshow('Black white image', dilated)
+cv2.imshow('Original image',originalImage)
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+#https://youtu.be/efWITgemKvs?t=1845
